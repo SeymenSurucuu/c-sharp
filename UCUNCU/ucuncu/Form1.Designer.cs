9b7e361e25b734de66c799f9a869d7c9d0a5170d
Seymen Sürücü <131146184+SeymenSurucuu@users.noreply.github.com>
2025-01-20 20:21:02 +0300

    Add files via upload

---
+++ UCUNCU/ucuncu/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace ucuncu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tcbilgisi = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dogumbil = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.telefonbil = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tcbilgisi
+            // 
+            this.tcbilgisi.Location = new System.Drawing.Point(30, 44);
+            this.tcbilgisi.Mask = "00000000000";
+            this.tcbilgisi.Name = "tcbilgisi";
+            this.tcbilgisi.Size = new System.Drawing.Size(165, 20);
+            this.tcbilgisi.TabIndex = 0;
+            this.tcbilgisi.ValidatingType = typeof(int);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(27, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TC Kimlik Numarası";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(27, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Doğum Tarihi";
+            // 
+            // dogumbil
+            // 
+            this.dogumbil.Location = new System.Drawing.Point(30, 94);
+            this.dogumbil.Mask = "00/00/0000";
+            this.dogumbil.Name = "dogumbil";
+            this.dogumbil.Size = new System.Drawing.Size(165, 20);
+            this.dogumbil.TabIndex = 2;
+            this.dogumbil.ValidatingType = typeof(System.DateTime);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(27, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Telefon Numarası";
+            // 
+            // telefonbil
+            // 
+            this.telefonbil.Location = new System.Drawing.Point(30, 159);
+            this.telefonbil.Mask = "(999) 000-0000";
+            this.telefonbil.Name = "telefonbil";
+            this.telefonbil.Size = new System.Drawing.Size(165, 20);
+            this.telefonbil.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 24);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Verileri İşle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(252, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(157, 184);
+            this.listBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(267, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Kullanıcı Verileri";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.DarkRed;
+            this.label5.Location = new System.Drawing.Point(165, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "*";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.DarkRed;
+            this.label6.Location = new System.Drawing.Point(132, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 18);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "*";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.DarkRed;
+            this.label7.Location = new System.Drawing.Point(180, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 18);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 258);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.telefonbil);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dogumbil);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tcbilgisi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox tcbilgisi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox dogumbil;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox telefonbil;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ UCUNCU/ucuncu/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ucuncu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // MaskCompleted ile kullanıcının yazdığı verinin doğru bir biçimde olup olmadığının kontrolü yapılır ve true false olarak tcTam, telTam, dogumTam verilerien atanır.
+               var tcTam = tcbilgisi.MaskCompleted;
+                var telTam = telefonbil.MaskCompleted;
+                 var dogumTam = dogumbil.MaskCompleted;
+
+
+            
+            //True false verileri için bool dizisi oluşturuyoruz ki giren kullanıcıya daha sonra hangi veriyi yanlış girdiğinin bilgisini verebilelim. 
+            bool[] kontrol = new bool[3];
+            kontrol[0] = tcTam;
+            kontrol[1] = telTam;
+            kontrol[2] = dogumTam;
+
+            //Kullanıcıya true false verisini gösterdiğimizde herhangi bir şey anlamayacağından kaynaklı bunlara karşılık gelen bir dizi açtık. 
+            string[] isimler = new string[] { "TC", "TELEFON", "DOĞUM TARİHİ" };
+
+
+            // isimler dizisi statik bir dizi olduğundan kaynaklı sonrasında verileri işlemesi zor oluyor bu yüzden bir liste açtım. Kullanıcı Telefon bilgisini yanlış girdi diyelim.
+            // Telefon bilgisi verisi false dönecek. Bizde bu false dönen veriyi isimler dizisi ile isimlendirip, bu ismi eksik veriler listesine atayacağız.
+            List<string> eksikVeriler = new List<string>();
+
+
+            // İsimler dizisi uzunluğunda bir döngü çalıştırıyoruz. Eğer kontrol dizisinde false varsa, bu veriyi eksikVeriler listesine ekliyoruz.
+             for (int i = 0; i < isimler.Length; i++)
+            {
+                if (kontrol[i] == false) // burada başta aldığımız verileri kontrol ettiriyoruz. Eğer yanlış ise gelen veri komutu çalışır. 
+                {
+                    eksikVeriler.Add(isimler[i]); // burada artık i kaçıncı sayıdaysa eksikveriler listesine, isimler içerisinde bulunan ve i' sayısına atanan değere(0,1,2) karşılık gelen veriyi ekler
+                }
+            }
+            if (tcTam == true  && telTam == true  && dogumTam == true)  // hepsi true ise gir
+            {
+                //verileri listbox1'e ekle
+                listBox1.Items.Add(tcbilgisi.Text);
+                listBox1.Items.Add(telefonbil.Text);
+                listBox1.Items.Add(dogumbil.Text);
+                listBox1.Items.Add("--------"); //veriler karışmasın babacım illa isim mi alıcaz
+            }
+            else
+            {
+                string eksikler = string.Join(", ", eksikVeriler); // eksikveriler listesindeki verilerin arasına virgül koyduk her zaman gelen veriyi bilemeyiz isimler dizisinde oynama yapmadan bu şekilde güncellenebilir.
+                MessageBox.Show("Lütfen Tüm Bilgileri Eksiksiz Doldurunuz! Yanlış Ya da Eksik Bilgiler: " + eksikler ); // gelen eskik verilerin ne olduğunu ekrana yazdık  
+                
+            }
+        }
+    }
+}
